09e2a42eb4fa65beab8941b2a7b3d6b2d9f47211
freshcodeAlice <freshjs.ofte@gmail.com>
2022-05-11 17:40:20 +0300

    Primary key

--- test-connection.session.sql
+++ test-connection.session.sql
@@ -1,8 +1,7 @@
 DROP TABLE users;
 
-/* CHECK (условие) */
-
 CREATE TABLE users(
+    id serial PRIMARY KEY,
     first_name varchar(64) NOT NULL CHECK (first_name != ''),
     last_name varchar(64) NOT NULL CHECK (last_name != ''),
     email varchar(256) NOT NULL UNIQUE CHECK (email != ''),
@@ -14,8 +13,29 @@ CREATE TABLE users(
 
 INSERT INTO users 
 VALUES ('Test', 'hello', 'test@test.com', 'male', true, '1999-05-10', 1.90),
-('world', 'Testovich', 'test1@test.com', 'female', true, '1999-05-10', 1.50),
+('world', 'Testovich', 'tes1t@test.com', 'female', true, '1999-05-10', 1.50),
 ('Test', 'Testovich', 'hw@com', 'binary', false, '1999-05-10', 1.90),
 ('Test', 'Testovich', 'test2@test.com', 'other', false, '1999-05-10', 1.80),
 ('Test', 'Testovich', 'teertertert', 'other', true, '1200-05-10', 0.3),
 ('Test', 'Testovich', 'test3@test.com', 'other', true, '1999-05-10', 2.10);
+
+
+
+DROP TABLE messages;
+
+CREATE TABLE messages(
+id serial PRIMARY KEY,
+body varchar(5000) NOT NULL,
+author varchar(64) NOT NULL,
+create_at timestamp DEFAULT current_timestamp,
+is_read boolean NOT NULL DEFAULT false
+);
+
+/* определяем порядок передачи значений*/
+
+INSERT INTO messages (author, body)
+ VALUES 
+('Test Testovich', 'message'),
+('Test Testovich', 'message'),
+('Test Testovich', 'message');
+
