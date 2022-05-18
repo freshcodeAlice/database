3e3b842e73cdf7aba2af0eb708c74fdc9203a344
freshcodeAlice <freshjs.ofte@gmail.com>
2022-05-18 20:04:23 +0300

    Filtering group, sort, text pattern

--- select.sql
+++ select.sql
@@ -305,3 +305,177 @@ GROUP BY "gender";
 
 
 */
+
+
+
+SELECT avg(weight), gender 
+FROM users
+GROUP BY gender;
+
+
+
+--  1. Посчитать количество телефонов, которые были проданы 
+--  (то есть количестве телефонов в заказах).
+ 
+ SELECT sum(quntity) FROM orders_to_phones;
+
+--  2. Количество телефонов, которые есть на "складе".
+SELECT sum(quntity) FROM phones;
+
+--  3. Средняя цена всех телефонов
+SELECT avg(price) FROM phones;
+
+--  4. Средняя цена каждого бренда
+SELECT avg(price), brand FROM phones
+GROUP BY brand;
+
+--  5. Стоимость всех товаров (цена*количество) 
+--  на складе в диапазоне их цен от 1000 до 2000.
+SELECT sum(quantity*price)
+FROM phones
+WHERE price BETWEEN 1000 AND 2000;
+
+
+--  6. Количество моделей каждого бренда
+SELECT count(*), brand FROM phones
+GROUP BY brand;
+
+
+-- проверяем
+SELECT * FROM phones
+WHERE brand = 'Nokia';
+
+--  7. Количество заказов каждого пользователя, который совершал заказы.
+SELECT count(*), user_id
+FROM orders
+GROUP BY user_id;
+ 
+--  8. Средняя цена телефонов Nokia.
+SELECT avg(price)
+FROM phones
+WHERE brand = 'Nokia';
+
+/*
+
+Фильтрация групп,
+Сортировка
+
+*/
+
+SELECT * 
+FROM phones
+ORDER BY quantity ASC
+LIMIT 10;
+
+
+-- Sorting
+
+/* 
+ORDER BY field {ASC|DESC}
+*/
+
+-- по высоте, возр.
+
+SELECT * 
+FROM users
+ORDER BY height ASC;
+
+-- по высоте, если высота одинаковая - то по дате рождения
+
+SELECT * 
+FROM users
+ORDER BY height ASC, birthday ASC ;
+
+--если и высота, и дата рождения одинаковые, отсортируй еще и по имени, по нисходящей
+SELECT * 
+FROM users
+ORDER BY height ASC, birthday ASC, first_name DESC ;
+
+
+
+/*
+2. Отсортируйте телефоны по цене, от самого дорогого до самого дешевого.
+*/
+SELECT * FROM phones
+ORDER BY price DESC;
+
+/*
+1. Отсортируйте пользователей по возрасту (по количеству полных лет) и по имени, если они одногодки.
+*/
+SELECT *, extract('year' from age(birthday)) AS "Age"
+FROM users
+ORDER BY extract('year' from age(birthday)), first_name ASC;
+
+--количестве одногодок
+
+SELECT count(*), "Age" 
+FROM
+(SELECT *, 
+extract('year' from age(birthday)) AS "Age"
+FROM users) AS "u_w_age"
+GROUP BY "Age"
+ORDER BY "Age";
+
+/*
+Фильтрация групп
+HAVING
+*/
+
+SELECT count(*), "Age" 
+FROM
+(SELECT *, 
+extract('year' from age(birthday)) AS "Age"
+FROM users) AS "u_w_age"
+GROUP BY "Age"
+HAVING count(*) >= 10
+ORDER BY count(*) DESC;
+
+/*
+Извлечь все бренды, у которых количество телефонов > 1000, отсортировать по алфавиту
+*/
+
+SELECT sum(quantity), brand
+FROM phones
+GROUP BY brand
+HAVING sum(quantity) > 1000;
+
+/*
+text pattern
+
+LIKE
+ILIKE
+
+SIMILAR TO
+
+~ -- регулярка
+~* -- регулярка с игнором кейза
+
+!~ - not 
+!~* - not с игнором кейза
+
+*/
+
+SELECT * FROM users
+WHERE first_name ILIKE 'A%'; --'Aba' 'aba'
+
+--'%' = *
+--'_' = .
+
+
+SELECT * FROM users
+WHERE first_name SIMILAR TO '%i{2}%';
+
+SELECT * FROM users
+WHERE first_name ~ '.*i{2}.*'; --регистрозависимый запрос
+
+SELECT * FROM users
+WHERE first_name ~* '.*i{2}.*'; --регистрозависимый запрос
+
+
+/*
+Среди всех сообщений в таблице "сообщения" найти слово "паровоз"
+*/
+
+SELECT * FROM "messages"
+WHERE body ILIKE '%паровоз%';
+
